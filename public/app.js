d4805974afc9c1046be1976a4b5527270993e9fd
jeyden94 <jeyden94@gmail.com>
2025-12-14 22:59:56 +0000

    Update - 0bc86ef9

--- public/app.js
+++ public/app.js
@@ -7,6 +7,33 @@
     // Camera
     var camera = setupCamera(scene, canvas);
 
+    // Pointer
+    scene.onPointerObservable.add((pointerInfo) => {
+      switch (pointerInfo.type) {
+        case BABYLON.PointerEventTypes.POINTERDOWN:
+          console.log("POINTER DOWN");
+          break;
+        case BABYLON.PointerEventTypes.POINTERUP:
+          console.log("POINTER UP");
+          break;
+        case BABYLON.PointerEventTypes.POINTERMOVE:
+          console.log("POINTER MOVE");
+          break;
+        case BABYLON.PointerEventTypes.POINTERWHEEL:
+          console.log("POINTER WHEEL");
+          break;
+        case BABYLON.PointerEventTypes.POINTERPICK:
+          console.log("POINTER PICK");
+          break;
+        case BABYLON.PointerEventTypes.POINTERTAP:
+          console.log("POINTER TAP");
+          break;
+        case BABYLON.PointerEventTypes.POINTERDOUBLETAP:
+          console.log("POINTER DOUBLE-TAP");
+          break;
+      }
+    });
+
     // Light
     const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1, 1, 1), scene);
     light.intensity = 0.5;  // reduce intensity    
