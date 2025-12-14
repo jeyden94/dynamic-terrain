e1d2a42b7d933b8434256babd71e6fdbe0c5c1e7
jeyden94 <jeyden94@gmail.com>
2025-12-14 19:28:04 +0000

    Update - 76e8fa7c

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  
+  let contentType = 'text/html';
+  if (filePath.endsWith('.js')) contentType = 'application/javascript';
+  if (filePath.endsWith('.css')) contentType = 'text/css';
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('<h1>404 - File Not Found</h1>');
+      return;
+    }
+    
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
