6869e4f4ffc18aa2c72fe409f5938363433a9250
jeyden94 <jeyden94@gmail.com>
2025-12-14 23:03:11 +0000

    Update - 2b06098a

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
     import { setupCamera } from './camera.js';
-    
+    import { trackPointer } from './pointer.js';
+
     const canvas = document.getElementById('renderCanvas');
     const engine = new BABYLON.Engine(canvas, true);
     const scene = new BABYLON.Scene(engine);
@@ -8,31 +9,32 @@
     var camera = setupCamera(scene, canvas);
 
     // Pointer
-    scene.onPointerObservable.add((pointerInfo) => {
-      switch (pointerInfo.type) {
-        case BABYLON.PointerEventTypes.POINTERDOWN:
-          console.log("POINTER DOWN");
-          break;
-        case BABYLON.PointerEventTypes.POINTERUP:
-          console.log("POINTER UP");
-          break;
-        case BABYLON.PointerEventTypes.POINTERMOVE:
-          console.log("POINTER MOVE");
-          break;
-        case BABYLON.PointerEventTypes.POINTERWHEEL:
-          console.log("POINTER WHEEL");
-          break;
-        case BABYLON.PointerEventTypes.POINTERPICK:
-          console.log("POINTER PICK");
-          break;
-        case BABYLON.PointerEventTypes.POINTERTAP:
-          console.log("POINTER TAP");
-          break;
-        case BABYLON.PointerEventTypes.POINTERDOUBLETAP:
-          console.log("POINTER DOUBLE-TAP");
-          break;
-      }
-    });
+    var pointer = trackPointer(scene);
+    // scene.onPointerObservable.add((pointerInfo) => {
+    //   switch (pointerInfo.type) {
+    //     case BABYLON.PointerEventTypes.POINTERDOWN:
+    //       console.log("POINTER DOWN");
+    //       break;
+    //     case BABYLON.PointerEventTypes.POINTERUP:
+    //       console.log("POINTER UP");
+    //       break;
+    //     case BABYLON.PointerEventTypes.POINTERMOVE:
+    //       console.log("POINTER MOVE");
+    //       break;
+    //     case BABYLON.PointerEventTypes.POINTERWHEEL:
+    //       console.log("POINTER WHEEL");
+    //       break;
+    //     case BABYLON.PointerEventTypes.POINTERPICK:
+    //       console.log("POINTER PICK");
+    //       break;
+    //     case BABYLON.PointerEventTypes.POINTERTAP:
+    //       console.log("POINTER TAP");
+    //       break;
+    //     case BABYLON.PointerEventTypes.POINTERDOUBLETAP:
+    //       console.log("POINTER DOUBLE-TAP");
+    //       break;
+    //   }
+    // });
 
     // Light
     const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1, 1, 1), scene);
