1eba927d45d4c081258e6237990be5178623055f
jeyden94 <jeyden94@gmail.com>
2025-12-14 19:55:00 +0000

    Update - 03fed21a

--- public/app.js
+++ public/app.js
@@ -9,14 +9,26 @@
     // Light
     const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
     
+    // map creation
+    var mapSubX = 100;
+    var mapSubZ = 100;
+    var mapData = new Float32Array(mapSubX * mapSubZ * 3);   
+    for (var l = 0; l < mapSubZ; l++) {           
+        for (var w = 0; w < mapSubX; w++) {                
+            mapData[3 *(l * mapSubX + w)] = (w - mapSubX * 0.5) * 2.0;
+            mapData[3 * (l * mapSubX + w) + 1] = w / (l +1) * Math.sin(l / 2) * Math.cos(w / 2) * 2.0;
+            mapData[3 * (l * mapSubX + w) + 2] = (l - mapSubZ * 0.5) * 2.0;
+        }            
+    }
+
     // Create a sphere
     const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene);
     sphere.position.y = 1;
     
     // Material for the sphere
-    const material = new BABYLON.StandardMaterial('sphereMat', scene);
-    material.diffuse = new BABYLON.Color3(0.2, 0.5, 1);
-    sphere.material = material;
+    // const material = new BABYLON.StandardMaterial('sphereMat', scene);
+    // material.diffuse = new BABYLON.Color3(0.2, 0.5, 1);
+    // sphere.material = material;
     
     // Render loop
     engine.runRenderLoop(() => {
