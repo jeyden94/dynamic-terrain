5e02e8d4ac82d6a5aa0f8ef887f7d210ec0208a2
jeyden94 <jeyden94@gmail.com>
2025-12-15 01:25:43 +0000

    Update - 5ac7ebae

--- public/camera.js
+++ public/camera.js
@@ -2,14 +2,14 @@ export function setupCamera(scene, canvas) {
 
 	var camera = new BABYLON.ArcRotateCamera("Camera", 0, 10, 10, new BABYLON.Vector3(0, 0, 0), scene);
 
-    // This positions the camera
-    camera.setPosition(new BABYLON.Vector3(0, 0, -10));
+  // This positions the camera
+  camera.setPosition(new BABYLON.Vector3(0, 0, -10));
 
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
-    
-    camera.lowerRadiusLimit = 6;
-    camera.upperRadiusLimit = 60;
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
+  
+  camera.lowerRadiusLimit = 6;
+  camera.upperRadiusLimit = 60;
 
-  return camera;
+  // return camera;
 }
