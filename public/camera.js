bae905b1d0ae7af359beaf52ccac4f519664b61f
jeyden94 <jeyden94@gmail.com>
2025-12-15 02:55:43 +0000

    Update - 9ed5efbd

--- public/camera.js
+++ public/camera.js
@@ -14,40 +14,7 @@ export function setupCamera(scene, canvas) {
   // Gui to move camera with mouse 
   const gui = new BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI('ui');   
 
-    var FreeCameraMouseInput = function () {
-      this._disableEdgeScroll = () => {
-          this._topEdgeScroll = false;
-          this._rightEdgeScroll = false;
-          this._bottomEdgeScroll = false;
-          this._leftEdgeScroll = false;
-      }
-
-      // enable edge areas
-      this._enabled = true;
-
-      // state, where the mouse is
-      this._topEdgeScroll = false;
-      this._rightEdgeScroll = false;
-      this._bottomEdgeScroll = false;
-      this._leftEdgeScroll = false;
-
-      // transparency of the edge areas
-      this._alphaEdgeScroll = 1.0;
-
-      // width/height in percentage of the edge areas
-      this._widthEdgeScroll = 0.05;
-      this._heightEdgeScroll = 0.05;
-
-      // gui elements for edge areas
-      this.topEdge = new BABYLON.GUI.Rectangle();
-      this.topRightCorner = new BABYLON.GUI.Rectangle();
-      this.rightEdge = new BABYLON.GUI.Rectangle();
-      this.bottomRightCorner = new BABYLON.GUI.Rectangle();
-      this.bottomEdge = new BABYLON.GUI.Rectangle();
-      this.bottomLeftCorner = new BABYLON.GUI.Rectangle();
-      this.leftEdge = new BABYLON.GUI.Rectangle();
-      this.topLeftCorner = new BABYLON.GUI.Rectangle();
-  };
+  
 
 
 }
